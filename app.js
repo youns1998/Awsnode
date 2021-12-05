9f6fd505ed139044e5c3f7d934ee67955e80149e
youns1998 <youns1998@gmail.com>
2021-12-06 03:30:29 +0900

    first connect

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var AWS = require('aws-sdk');
+AWS.config.region = "ap-northeast-2";
+var ec2 = new AWS.EC2();
+ec2.describeInstances({}, function(err, data){
+	console.log(data);
+	console.log(err);
+});
 app.get('/', function(req, res){
         res.send('Hello world');
 });
-app.listen(80, function(){
-        console.log('Connect 80 port');
-});
+//app.listen(90, function(){
+//        console.log('Connect 90 port');
+//});
